2e011ddfee2f8bb97eacef9ceb865f508e370022
Matheus <playermaster10like@gmail.com>
2020-10-15 22:54:04 -0300

    segundo teste

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const clicouEmTrocar= (evento) =>{
     evento.preventDefault();
 
-    
+    let imagem2= document.getElementById('foto-natureza');
+    imagem2.src= 'download.jpg';
 };
 
 const iniciar = ()=> {
